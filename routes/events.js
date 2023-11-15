362e4f8c1641d6a8be0012c3fd8a9bd2eefb9e0c
jareyes <josh@joshreyes.com>
2023-11-15 11:26:57 -0500

    Add ical support for events, secretly, silently

--- routes/events.js
+++ routes/events.js
@@ -4,23 +4,26 @@ const {Router} = require("express");
 const User = require("../lib/user");
 
 async function signup(req, res, next) {
-  const form = req.body;
-  const email = form.email;
-  const event_id = form.event_id;
+  try {
+    const form = req.body;
+    const email = form.email;
+    const event_id = form.event_id;
 
-  let user = await User.get_by_email(email);
-  if(user === null) {
-    user = User.create(email);
-    await user.save()
+    let user = await User.get_by_email(email);
+    if(user === null) {
+      user = User.create(email);
+      await user.save()
+    }
+
+    const reservation = Reservation.create(user.user_id, event_id);
+    await reservation.save();
+
+    const event = await Event.get_by_id(event_id);
+    res.redirect(`signup/${event.slug}`);
   }
-  console.log("user", user, "event_id", event_id);
-  const reservation = Reservation.create(user.user_id, event_id);
-  console.log(reservation);
-  await reservation.save();
-
-  const event = await Event.get_by_id(event_id);
-  console.log("event_id", event_id, "event", event);
-  res.redirect(`signup/${event.slug}`);
+  catch(err) {
+    next(err);
+  }
 }
 
 function signup_success(req, res) {
@@ -28,12 +31,29 @@ function signup_success(req, res) {
   res.render(`events/success-${slug}`);
 }
 
+async function ical_event(req, res) {
+  try {
+    const slug = req.params.slug;
+    const event = await Event.get_by_slug(slug);
+
+    console.log("event", event);
+
+    res.header("Content-Type", "text/calendar");
+    res.render("event-ical", {...event, layout: null});
+  }
+  catch(err) {
+    next(err);
+  }
+}
+
 const router = new Router();
 router.get("/", (req, res) => res.render("events"));
+
 router.get("/:slug", (req, res) => {
   const slug = req.params.slug;
   res.render(`events/${slug}`);
 });
+router.get("/:slug/invite.ics", ical_event);
 
 router.post("/signup", signup);
 router.get("/signup/:slug", signup_success);
