ab162a7f7e4b9f3938b2e6e411ed88af1c4ceef0
jareyes <josh@joshreyes.com>
2023-11-18 10:19:02 -0500

    Placeholder details for upcoming playdate

---
+++ migrations/013-playdate-details.sql
@@ -0,0 +1,1 @@
+UPDATE events SET details="<h2>The Play</h2><p>We haven't picked a script yet. But as soon as we decide on one, we'll let you know here.</p><p>It'll be good, though. Promise.</p>" WHERE event_id=4;
