d5691269166f08d11138bbfe676372a3036d9689
jareyes <josh@joshreyes.com>
2024-01-19 13:50:47 -0500

    Added Lysistrata

---
+++ migrations/014-event-playdate-lysistrata.sql
@@ -0,0 +1,26 @@
+INSERT INTO events (event_id, event_date, title, slug, start_datetime, calendar_desciption, calendar_uuid, duration_m, location_url, summary, details, location_display, location_address)
+VALUES (
+  5,
+  "2024-01-30 23:00:00",
+  "Playdate: Lysistrata",
+  "2024-01-30-playdate",
+  "2024-01-30 23:00:00",
+  "Use the entrace by the police station. Take the elevator to the fifth floor, and join us on the stage.\n\nLight beverages, snacks, and scripts provided.",
+  "3b08795c-05a1-4f0f-a842-bb93ed0783c4",
+  120,
+  "https://goo.gl/maps/ogR2jWQBGRZ3TKGcA",
+  "<p>
+    This month we're reading an old comedy classic: <i>Lysistrata</i>. The woman of Greece are sick of the ongoing Peloponnesian War and decide to stop it by stop having sex with their husbands. It's a lot of fun. Come join us!
+  </p>
+  <p>
+    No acting experience required. Water and light snacks provided.
+  </p>",
+  "<h2>About Lysistrata</h2>
+  <p>This time we're reading a classic by the Greek comedian Aristophanes. Written nearly 2,500 years ago, <i>Lysistrata</i> is about one woman who has had enough with the Peloponnesian war and decides its time to take matters in her own hands: she organizes the woman of Greece to go on a sex strike until the men stop their silly war.</p>
+
+  <p>This is one of the first plays about organized labor, collective bargaining, and anti-war protests. But it's also full of sex jokes.</p>
+
+  <p>You'll have fun. Promise.</p>",
+  "Claremont Opera House, Claremont, NH",
+  "Claremont Opera House, 58 Opera House Square, Claremont, NH"
+);
