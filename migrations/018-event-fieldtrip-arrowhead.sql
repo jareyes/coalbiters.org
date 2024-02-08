8936056349e719b7670958b87fe70e06affa1716
jareyes <josh@joshreyes.com>
2024-02-08 10:23:18 -0500

    Field trip: Arrowhead

---
+++ migrations/018-event-fieldtrip-arrowhead.sql
@@ -0,0 +1,29 @@
+INSERT INTO events (event_id, event_date, title, slug, start_datetime, calendar_description, calendar_uuid, duration_m, location_url, summary, details, location_display, location_address)
+VALUES (
+  7,
+  "2024-02-23 23:00:00",
+  "Field Trip: Snow Tubing",
+  "2024-02-23-fieldtrip",
+  "2024-02-23 23:00:00",
+  "Park in the lot and pay for and pick up your bracelets at the ticket booth. Cost of admission is $12 per person, including small children.",
+  "53810ce6-f4f2-47bb-9a4c-b28d5e2c81f9",
+  180,
+  "https://maps.app.goo.gl/RfmRj4Zg7fzJz6DG9",
+  "<p>
+  We're going to Arrowhead Recreation Area to snow tube. Feel the rush as you careen under the lights down a lane of snow and ice. It's fast. It's furious. It's fun. And it's even better because it's at night!
+  </p>",
+  "<h2>Details</h2>
+  <p>
+    Arrowhead Recreation Area is a volunteer run, one-stop shop for outdoor activities in Claremont, NH. During the winter they offer affordable skiing, snowboarding, and snow tubing&mdash;weather permitting.
+  </p>
+
+  <p>
+    Free parking at the facility. Tickets are $12 per person, including small children. The cost of admission gets you unlimited tubing all night long. And they'll even tow you up to the top of the hill. Food and beverage concessions are available in the cabin.
+  </p>
+
+  <p>
+    We'll start at the beginning of the night to get as much tubing in as possible. But you're welcome to come anytime!
+  </p>",
+  "Arrowhead Recreation Area, Claremont, NH",
+  "Arrowhead Recreation Area, 18 Robert Easter Way, Claremont, NH 03743"
+);
