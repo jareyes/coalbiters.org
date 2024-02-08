809014865d93722ee8085f41fd702cd48cd57b56
jareyes <josh@joshreyes.com>
2024-02-08 09:57:24 -0500

    Field Trip: Hood

---
+++ migrations/016-event-fieldtrip-hood.sql
@@ -0,0 +1,25 @@
+INSERT INTO events (event_id, event_date, title, slug, start_datetime, calendar_description, calendar_uuid, duration_m, location_url, summary, details, location_display, location_address)
+VALUES (
+  6,
+  "2024-02-17 19:00:00",
+  "Field Trip: Hood Museum of Art",
+  "2024-02-17-fieldtrip",
+  "2024-02-17 19:00:00",
+  "Join us for in-person tours of the Hood Museum galleries. Meet in the Russo Atrium five minutes prior to the start time.",
+  "af902db1-636b-41cc-9fca-eb90c0819ddc",
+  60,
+  "https://maps.app.goo.gl/oQm5JnjM8Q2SqYf79",
+  "<p>
+  We're going on a field trip! Join us for an in-person guided tour the galleries at the Hood Musuem of Art. It's free. It's fun. It's art. You're art. Come join us.
+  </p>",
+  "<h2>About the Hood Museum of Art</h2>
+  <p>
+Join us our first Field Trip at the Hood. The Hood Museum of Art houses the art collection of Dartmouth College. Admission is free and open to the public.</p>
+
+  <p>The <a href=\"https://hoodmuseum.dartmouth.edu/explore/collection\">collections of the museum</a> span multiple geographies, times, and styles. Here you can find treasures like ninth-century BCE Assyrian stone reliefs and the fresco mural cycle <a href=\"https://hoodmuseum.dartmouth.edu/explore/collection/featured-collections/orozcos-epic\">The Epic of American Civilization</a> by José Clemente Orozco. There's a little bit for everyone here.
+  </p>
+
+  <p>We'll go on an hour-long guided tour that highlights several pieces of the collection. Then you're free to explore with other Coalbiters or on your own.</p>",
+  "Hood Museum of Art, Hanover, NH",
+  "Hood Museum of Art, 6 E Wheelock St, Hanover, NH 03755"
+);
