11b925c40e8a55f008ac6bd8d2723ad6c400fc1d
jareyes <josh@joshreyes.com>
2024-04-06 12:41:20 -0400

    Daddy's Friday Night Hang

---
+++ migrations/022-hang-daddys.sql
@@ -0,0 +1,25 @@
+INSERT INTO events (
+  event_id, title, slug, start_time, duration_m, calendar_uuid, location_url, venue, venue_address, directions, summary, details
+)
+VALUES (
+  8,
+  "Friday Night Hang",
+  "2024-04-12-hang",
+  "2024-04-12 23:00:00",
+  180,
+  "f2aeac99-a09f-4b18-a712-69b0f37d2aed",
+  "https://maps.app.goo.gl/nfevzxMBLQVSi4Zr7",
+  "Daddy's Pizza",
+  "50 Pleasant Street, Claremont, NH 03734",
+  "Come hang out with other Coalbiters at Daddy's Pizza Friday night. Grab a drink, grab a slice, answer some trivia, and laugh. There is free public parking on Pleasant Street.",
+  "<p>
+  It's been a long week. Come hang with some friendly faces downtown. We're going to get together at Daddy's Pizza starting at 7pm for drinks. If you're hungry, put in a food order. We'll bring a few decks of easy trivia, so that no one has to think too hard about what to say.
+  </p>",
+  "<h2>About Daddy's Pizza</h2>
+  <p>
+  Located in downtown Claremont on Pleasant Street, Daddy's is easy to get to with ample public parking. They have a full bar and kitchen. I recommend the tuna melt and turkey club sandwiches.
+  </p>
+  <p>
+  Hangs have no agenda. We'll have a few decks of trivia and some playing cards to help relax and pass the time. Come and go as you please. But please stop by, we'd love to see you.
+  </p>"
+);
