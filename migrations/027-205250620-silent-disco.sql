f9d25b43a76a96942e20cfed43669ec688470952
Josh Reyes <howdy@coalbiters.org>
2025-06-01 12:08:35 -0400

    Pride Silent Disco

---
+++ migrations/027-205250620-silent-disco.sql
@@ -0,0 +1,29 @@
+INSERT INTO events (
+   event_id,
+   title,
+   slug,
+   start_time,
+   duration_m,
+   calendar_uuid,
+   location_url,
+   venue,
+   venue_address,
+   directions,
+   summary,
+   stripe_price_id,
+   price
+) VALUES (
+  13,
+  "Silent Disco",
+  "2025-06-20-silent-disco",
+  "2025-06-20 23:00:00",
+  180,
+  "e01dcdb1-1ec1-4a74-a6ee-1b3152754088",
+  "https://maps.app.goo.gl/LgEdxygfoLt53TSA8",
+  "Claremont Country Club",
+  "53 Country Club Road, Claremont, NH 03743",
+  "Turn up the driveway and continue until you hit the club house. Park anywhere in the lot. We'll see you inside!",
+  "It's that time of the month again! Grab a headset and dance like there's nobody watching. Switch between three channels of music all night long. The Country Club has a full bar and free parking. Rest in between songs with your fellow Coalbiter friends.",
+  "price_1QsltRDBDUSJ7VKxQYl4cttC",
+  1200
+);
