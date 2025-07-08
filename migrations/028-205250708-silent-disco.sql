e1f06585a31df205e5921cbf6561e73bb67728c1
Josh Reyes <howdy@coalbiters.org>
2025-07-08 19:41:13 -0400

    July disco

---
+++ migrations/028-205250708-silent-disco.sql
@@ -0,0 +1,29 @@
+INSERT INTO events (
+   event_id,
+   title,
+   slug,
+   start_time,
+   duration_m,
+   calendar_uuid,
+   location_url,
+   venue,
+   venue_address,
+   directions,
+   summary,
+   stripe_price_id,
+   price
+) VALUES (
+  14,
+  "Silent Disco",
+  "2025-07-25-silent-disco",
+  "2025-07-25 23:00:00",
+  180,
+  "4d6cea08-0672-4b63-8dd8-7df36e438c24",
+  "https://maps.app.goo.gl/LgEdxygfoLt53TSA8",
+  "Claremont Country Club",
+  "53 Country Club Road, Claremont, NH 03743",
+  "Turn up the driveway and continue until you hit the club house. Park anywhere in the lot. We'll see you inside!",
+  "It's that time of the month again! Grab a headset and dance like there's nobody watching. Switch between three channels of music all night long. The Country Club has a full bar and free parking. Rest in between songs with your fellow Coalbiter friends.",
+  "price_1QsltRDBDUSJ7VKxQYl4cttC",
+  1200
+);
